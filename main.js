4c825fd3b6705685a7a2e4b1b4ff6d5ce9c25599
ahoua900 <elvisahoua64@gmail.com>
2021-07-15 09:31:27 +0100

    first commit frontend

---
+++ main.js
@@ -0,0 +1,10 @@
+function Onblack(){
+var elt = document.getElementById('header');
+elt.style.backgroundColor = "white";
+elt.style.color = "black";
+}
+function Onlight(){
+var elt = document.getElementById('header');
+elt.style.backgroundColor = "black";
+elt.style.color = "white";
+}
